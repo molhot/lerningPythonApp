81b6efc81e0f58966bb5e6e632e55d4ea8e82c94
molhot <sakata19991214@gmail.com>
2025-10-12 07:37:47 -0700

    ヘッダー部分のデザインを修正 画面幅に合わせて文字のサイズが変更なるように変更

--- src/resources/views/userAuticate/login.blade.php
+++ src/resources/views/userAuticate/login.blade.php
@@ -1,43 +1,35 @@
+@php
+$noExistUnderHeader = true;
+@endphp
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="{{ asset('/css/common/header.css') }}" rel="stylesheet"  type="text/css"  media="all">
+        <link href="{{ asset('/css/login.css') }}" rel="stylesheet"  type="text/css"  media="all">
+    </head>
+
 @include('/layouts/header/header')
 
-<!-- (layouts/header.php) -->
-<!-- <body> -->
-<header>
-    <div id="header">
-        <div id="headerIcon">
-            <img src="{{ asset('/images/headerIcon.png') }}">
-        </div>
-        <div id="headerText">PythonLerningApp</div>
-        <div id="headerIcon">
-            <img src="{{ asset('/images/headerIcon.png') }}">
-        </div>
-    </div>
-</header>
-
-<div id="underHeader">
-    <div class="pageHeaderBar">
-        <img class="barImage" src="{{ asset('/images/barImage.png') }}">
-        <div class="barText">LoginPage</div>
-    </div>
-
+<div id="loginFormContainer">
     <form id="loginForm" action="{{url('/login')}}" method="post">
         @csrf
         <div id="loginPage">
             <div class="loginArea">
-                <div class="loginArea">emailAddress</div>
+                <div class="loginAreaHolder">emailAddress</div>
                 <div class="inputArea">
                     <input type="text" id="emailAddress" name="emailAddress" placeholder="メールアドレスを入力">
                 </div>
             </div>
             <div class="loginArea">
-                <div class="loginArea">パスワード</div>
+                <div class="loginAreaHolder">パスワード</div>
                 <div class="inputArea">
                     <input type="text" id="password" name="password" placeholder="パスワードを入力">
                 </div>
             </div>
         </div>
 
-        <div id="loginArea">
+        <div id="loginSubmmitArea">
             <div id="loginActionButtonArea">
                 <div id="loginButton">
                     <img src="../common/assets/registButton.png">
@@ -48,7 +40,9 @@
                     </button>
                 </div>
             </div>
-            <hr>
+            <div id="contentSeparator">
+                <hr>
+            </div>
         </div>
     </form>
 </div>
@@ -59,4 +53,5 @@
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="{{ asset('/js/login/login.js') }}"></script>
+</html>
 <!-- </body> -->
